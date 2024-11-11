516aa7b2b24d0ba9457a10585f0e2f1b32334bed
maxim <korobov.maksim6@wb.ru>
2024-11-11 23:34:40 +0300

    add some files

---
+++ restaurant-ui/src/App.js
@@ -0,0 +1,30 @@
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Sidebar from './components/Sidebar'; // Импортируем Sidebar
+import Dishes from './components/Dishes';
+import Employees from './components/Employees';
+import Products from './components/Products';
+import Customers from './components/Customers';
+import Content from './components/Content'; // Импортируем Content для отображения контента
+
+const App = () => {
+    return (
+        <Router>
+            <div className="app">
+                <Sidebar /> {/* Боковая панель */}
+                <Content>
+                    <Routes>
+                        <Route path="/" element={<h1>Welcome to Restaurant Management</h1>} />
+                        <Route path="/dishes" element={<Dishes />} />
+                        <Route path="/employees" element={<Employees />} />
+                        <Route path="/products" element={<Products />} />
+                        <Route path="/customers" element={<Customers />} />
+                    </Routes>
+                </Content>
+            </div>
+        </Router>
+    );
+};
+
+export default App;
